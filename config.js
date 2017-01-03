689cad97d2cfa12ca53b77f155196c4b9d6fdc11
Scott Spence <scottspence10@gmail.com>
2017-01-03 06:59:58 +0000

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: '29fXpt7iD2QNb6YfrQ0o3pEBm',  
+  consumer_secret: '9CjOMNCkEQmxq1eOGOIsU2f3zBRrmqaukZwpIZh4qGTvgpvFnC',
+  access_token: '4897735439-sOrGmZj7zuzl4psEcC4o9ymM3hLJ6D7v77ojwMR',  
+  access_token_secret: 'Qo3ZXLVfqWssKGFrqdt1XYJX0mxfA4lMLEkUHei8W8OOX'
+}
