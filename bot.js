f13362becbf0226b38efb08e6c1771664988d865
Scott Spence <scottspence10@gmail.com>
2017-01-03 14:56:58 +0000

    added new keys changed retweet and follow times and added follow back

--- bot.js
+++ bot.js
@@ -5,12 +5,17 @@ var
     
 var Twitter = new twit(config);
 
+// https://dev.twitter.com/rest/reference/get/search/tweets
+// A UTF-8, URL-encoded search query of 500 characters maximum, including operators. 
+// Queries may additionally be limited by complexity.
+var queryString = '#100DaysOfCode, #100daysofcode, #GitHub, #github';
+
 // RETWEET BOT ==========================
 
 // find latest tweet according the query 'q' in params
 var retweet = function() {
     var params = {
-        q: '#nodejs, #Nodejs, #100DaysOfCode, #mongodb',  // REQUIRED
+        q: queryString,  // REQUIRED
         result_type: 'recent',
         lang: 'en'
     }
@@ -28,7 +33,7 @@ var retweet = function() {
                 }
                 // if there was an error while tweeting
                 if (err) {
-                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
+                    console.log('Something went wrong while RETWEETING... Duplication maybe...: ' + err);
                 }
             });
         }
@@ -41,20 +46,20 @@ var retweet = function() {
 
 // grab & retweet as soon as program is running...
 retweet();
-// retweet in every 50 minutes
-// setInterval(retweet, 3000000);
-// retweet in every five minutes
-setInterval(retweet, 3000000);
+// retweet in every ten minutes
+setInterval(retweet, 600000);
 
 // FAVORITE BOT====================
 
 // find a random tweet and 'favorite' it
 var favoriteTweet = function(){
   var params = {
-      q: '#nodejs, #Nodejs',  // REQUIRED
+      q: queryString,  // REQUIRED
       result_type: 'recent',
       lang: 'en'
-  }
+  };
+  
+  
   // find the tweet
   Twitter.get('search/tweets', params, function(err,data){
 
@@ -68,7 +73,7 @@ var favoriteTweet = function(){
       Twitter.post('favorites/create', {id: randomTweet.id_str}, function(err, response){
         // if there was an error while 'favorite'
         if(err){
-          console.log('CANNOT BE FAVORITE... Error');
+          console.log('CANNOT BE FAVORITE... Error: ' + err);
         }
         else{
           console.log('FAVORITED... Success!!!');
@@ -76,16 +81,49 @@ var favoriteTweet = function(){
       });
     }
   });
-}
+};
+
 // grab & 'favorite' as soon as program is running...
 favoriteTweet();
-// 'favorite' a tweet in every 60 minutes
-// setInterval(favoriteTweet, 3600000);
-// 'favorite' a tweet in every 30 minutes
-setInterval(favoriteTweet, 1800000);
+// 'favorite' a tweet in every ten minutes
+setInterval(favoriteTweet, 600000);
+
+// STREAM API for interacting with a USER =======
+// set up a user stream
+var stream = Twitter.stream('user');
+
+// REPLY-FOLLOW BOT ============================
+// what to do when someone follows you?
+stream.on('follow', followed);
+
+// ...trigger the callback
+function followed(event) {
+  console.log('Follow Event now RUNNING');
+  // get USER's twitter handler (screen name)
+  var name = event.source.name,
+      screenName = event.source.screen_name;
+  // function that replies back to every USER who followed for the first time
+  tweetNow('@' + screenName + ' Thanks for the follow! What are you working on today?');
+}
+
+// function definition to tweet back to USER who followed
+function tweetNow(tweetTxt) {
+  var tweet = {
+    status: tweetTxt
+  };
+  Twitter.post('statuses/update', tweet, function (err,data, response) {
+    if(err){
+      console.log('Cannot Reply to Follower. ERROR!: ' + err);
+    }
+    else{
+      console.log('Reply to follower. SUCCESS!');
+    }
+  });
+}
 
 // function to generate a random tweet tweet
 function ranDom (arr) {
   var index = Math.floor(Math.random()*arr.length);
+  console.log('arr: ' + index);
   return arr[index];
-};
+}
--- config.js
+++ config.js
@@ -6,8 +6,8 @@
  * access_token_secret
  */
 module.exports = {
-  consumer_key: '29fXpt7iD2QNb6YfrQ0o3pEBm',  
-  consumer_secret: '9CjOMNCkEQmxq1eOGOIsU2f3zBRrmqaukZwpIZh4qGTvgpvFnC',
-  access_token: '4897735439-sOrGmZj7zuzl4psEcC4o9ymM3hLJ6D7v77ojwMR',  
-  access_token_secret: 'Qo3ZXLVfqWssKGFrqdt1XYJX0mxfA4lMLEkUHei8W8OOX'
+  consumer_key: 'IjKHDGqGLwF8RH0sQG88K7T91',  
+  consumer_secret: 'dmEy9F82MJUunKc3GfAFxnkC7KQxDQ33M9tpb5JxE7Qiwj8Lwm',
+  access_token: '4897735439-3iP6UTyGkXyr0MlC55b4rtXUIxqJY6ET3qHASNi',  
+  access_token_secret: 'gD68bF8YgZdHtv4vwuGOqGw8yia9cquxS1fuDNKYQ2X30'
 }
