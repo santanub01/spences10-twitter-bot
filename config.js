ef30da505b18962594e14de53bbebc4ad11eb478
Scott Spence <scottspence10@gmail.com>
2017-01-12 22:21:41 +0000

    change keys

--- config.js
+++ config.js
@@ -6,8 +6,8 @@
  * access_token_secret
  */
 module.exports = {
-  consumer_key: 'IjKHDGqGLwF8RH0sQG88K7T91',  
-  consumer_secret: 'dmEy9F82MJUunKc3GfAFxnkC7KQxDQ33M9tpb5JxE7Qiwj8Lwm',
+  consumer_key: 'gwHjgTk5mGjaHJsXRLgVmw1up',  
+  consumer_secret: 'UYIze7ZiVe7oQsvPCwYlmu4fM6JQqgiOc7RzvIb4ChqkC2KCG7',
   access_token: '4897735439-3iP6UTyGkXyr0MlC55b4rtXUIxqJY6ET3qHASNi',  
   access_token_secret: 'gD68bF8YgZdHtv4vwuGOqGw8yia9cquxS1fuDNKYQ2X30'
-}
+};
